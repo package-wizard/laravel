eb9328a2b8ced3ce9a4f0608fd4cc363aa441162
Andrey Helldar <helldar@ai-rus.com>
2021-02-15 22:54:56 +0300

    Update PackageStepper.php

--- src/PackageStepper.php
+++ src/PackageStepper.php
@@ -6,4 +6,15 @@ use Helldar\PackageWizard\Steppers\BaseStepper;
 
 final class PackageStepper extends BaseStepper
 {
+    protected array $require = [
+        'php' => '^8.0',
+
+        'illuminate/support' => '^8.0',
+    ];
+
+    protected array $require_dev = [
+        'mockery/mockery'     => '^1.0',
+        'orchestra/testbench' => '^6.0',
+        'phpunit/phpunit'     => '^9.0',
+    ];
 }
