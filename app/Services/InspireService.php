95619eee90783c07c6cb8cb16d5f4f8a463a2782
Andrey Helldar <helldar@dragon-code.pro>
2025-03-21 18:58:52 +0300

    Added cache example

--- app/Services/InspireService.php
+++ app/Services/InspireService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Data\QuoteData;
+use DragonCode\Cache\Services\Cache;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Collection;
 
@@ -15,16 +16,21 @@ class InspireService
     /** @return Collection<QuoteData> */
     public function quotes(): Collection
     {
-        return Inspiring::quotes()
-            ->shuffle()
-            ->take(5)
-            ->map(static function (string $quote) {
-                [$text, $author] = explode('-', $quote);
+        return Cache::make()
+            ->ttl(10, false)
+            ->key($this)
+            ->remember(function () {
+                return Inspiring::quotes()
+                    ->shuffle()
+                    ->take(5)
+                    ->map(static function (string $quote) {
+                        [$text, $author] = explode('-', $quote);
 
-                return QuoteData::from([
-                    'text'   => $text,
-                    'author' => $author,
-                ]);
+                        return QuoteData::from([
+                            'text'   => $text,
+                            'author' => $author,
+                        ]);
+                    });
             });
     }
 
